4010533f33c5a7477136718c54d8bdae549b5b50
d0w <derekxu888@gmail.com>
2024-04-08 18:03:10 -0400

    fix table

--- bostonhacks-admin-dashboard/src/app/data/page.js
+++ bostonhacks-admin-dashboard/src/app/data/page.js
@@ -3,11 +3,19 @@
 import Table from '@/components/Table'
 import React, { useEffect, useState } from 'react'
 
-const columns = [
+// add more fields to columns to account for more fields in the database
+// consider adding a global file that holds these columns
+const applicantColumns = [
     { field: 'id', headerName: 'ID' },
     { field: 'firstName', headerName: 'Name', width: 300 },
     { field: 'email', headerName: 'Email', width: 600 }
 ];
+
+const userColumns = [
+    { field: 'id', headerName: 'ID' },
+    { field: 'firstName', headerName: 'Name', width: 300 },
+    { field: 'email', headerName: 'Email', width: 600 }
+]
 
 const page = () => {
     const [applicants, setApplicants] = useState([]);
@@ -34,11 +42,11 @@ const page = () => {
     <div className="flex flex-col bg-white text-black p-5">
         <div className='m-5'>
             <p className="text-xl">Applicants (More fields to come)</p>
-            <Table rows={Object.values(applicants)} columns={columns} pageSize={25}/>
+            <Table rows={Object.values(applicants)} columns={applicantColumns} pageSize={25} pageSizeOptions={[5, 10, 20, 50, 100]}/>
         </div>
         <div className='m-5'>
             <p className='text-xl'>User (i think this means bhacks admins that control site)</p>
-            <Table rows={[]} columns={columns} pageSize={25} pageSizeOptions={[5, 10, 20, 50, 100]}/>
+            <Table rows={[]} columns={userColumns} pageSize={25} pageSizeOptions={[5, 10, 20, 50, 100]}/>
         </div>
     </div>
     
--- bostonhacks-admin-dashboard/src/components/Table.jsx
+++ bostonhacks-admin-dashboard/src/components/Table.jsx
@@ -5,17 +5,15 @@ import { DataGrid } from '@mui/x-data-grid';
 // Table component to list all the data
 // Utilizes material-ui's DataGrid component
 
-const Table = ({ rows, columns, pageSize=25, pageSizeOptions=[5, 10, 20, 50, 100] }) => {
+const Table = ({ rows, columns, pageSize=25, pageSizeOptions}) => {
   return (
     <DataGrid 
       rows={rows} 
       columns={columns} 
-      pageSize={pageSize} 
       pageSizeOptions={pageSizeOptions}
       initialState={{
         pagination: { paginationModel: { pageSize: pageSize } },
       }}
-      pagination 
     />
   )
 }
